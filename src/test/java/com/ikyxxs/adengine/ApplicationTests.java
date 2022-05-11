ee34a59bb45a1722781aa9244e3c08c4697bdde7
木白 <624104747@qq.com>
2022-05-11 14:20:44 +0800

    first commit

---
+++ src/test/java/com/ikyxxs/adengine/ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ikyxxs.adengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void test() {
+        System.out.println();
+    }
+}
